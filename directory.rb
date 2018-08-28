71f54b7435806a5409263210d725ad769cc815fa
olliebaum <oliver@baums.org.uk>
2018-08-29 00:30:26 +0100

    Modified directory.rb. Stores students in an array. Prints with each loop. Uses .length for student_count.

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
-student_count = 11
 #First the code prints a list of the students
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+students.each { |student| puts student }
+
 #Then it prints a count of the students
+student_count = students.length
 puts "Overall, we have #{student_count} great students"
