4ab4c87306274e4a4546c547ce54099dbdd74542
olliebaum <oliver@baums.org.uk>
2018-08-29 22:02:13 +0100

    Exercise 1. Changed print_list_of() to print names as numbered list.

--- directory.rb
+++ directory.rb
@@ -17,8 +17,8 @@ def print_header
 end
 
 def print_list_of(students)
-  students.each { |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) { |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   }
 end
 
