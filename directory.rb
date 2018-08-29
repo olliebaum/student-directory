31efb5957a5c04c6f5294d1262024f6b7948be5f
olliebaum <oliver@baums.org.uk>
2018-08-29 21:15:00 +0100

    Changed students into an array of hashes with keys :name and :cohort.

--- directory.rb
+++ directory.rb
@@ -1,28 +1,30 @@
 #Put the students in an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
   puts "Students of Villains Academy"
   puts "----------------------------"
 end
 
-def print_list_of(names)
-  names.each { |name| puts name }
+def print_list_of(students)
+  students.each { |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  }
 end
 
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
 end
 
 print_header
