913807f9af87c96d720783e94161d024ff294cd1
olliebaum <oliver@baums.org.uk>
2018-09-01 19:05:33 +0100

    Exercises 7 & 9. Asks user to input age, country + cohort. Student count uses pluralization.

--- directory.rb
+++ directory.rb
@@ -1,25 +1,41 @@
-#Put the students in an array
+def program_intro
+  puts
+  puts "WELCOME TO THE STUDENT DIRECTORY."
+  puts "Enter a list of students, and they will be printed for you."
+  puts
+end
+
 def input_students
   students = []
-  puts "Type the names of the students to add:"
-  puts "(Hit return twice to finish)"
+  puts "Enter details for the first student."
+  print "Name: ".rjust(10)
   while true
     name = gets.chomp
     break if name.empty?
-    students << {name: name, cohort: :september, country: "UK", age: "25", }
-    puts "#{students.count} students in total."
+    print "Age: ".rjust(10); age = gets.chomp.to_sym
+    print "Country: ".rjust(10); country = gets.chomp.to_sym
+    print "Cohort: ".rjust(10); cohort = gets.chomp.to_sym
+    age = :"??" if age == :""
+    country = :Unknown if country == :""
+    cohort = :September if cohort == :""
+    students << {name: name,  age: age, country: country, cohort: cohort}
+    puts "'#{name}' added. #{students.count} " +
+    (students.count == 1 ? "student" : "students") + " in total."
+    puts "Input a new student's name (or hit enter to finish):"
   end
   return students
 end
+
 def print_header
   puts "Students of Villains Academy"
-  puts "----------------------------"
+  puts "-----------------------------------------------"
 end
 
 def ask_for_letter
   print "Print students beginning with which letter? "
   letter = gets.chomp
 end
+
 def print_list_of(students, letter)
   students.select{|student|
     student[:name].chars.first.downcase == letter.downcase && student[:name].length < 12
@@ -28,18 +44,26 @@ def print_list_of(students, letter)
   }
   puts "(N.B. Students with 12 or more characters in their name were omitted.)"
 end
+
 def print_list_of_all(students)
   i = 0
   puts "#.".ljust(4) + "Name".ljust(20) + "Age".ljust(4) + "Country".ljust(11) + "Cohort".ljust(11)
   while i < students.count
-    puts "#{i+1}.".ljust(4) + students[i][:name].ljust(20) + students[i][:age].ljust(4) + students[i][:country].ljust(11) + "#{students[i][:cohort]}".ljust(11)
+    puts "#{i+1}.".ljust(4) +
+    "#{students[i][:name]}".ljust(20) +
+    "#{students[i][:age]}".ljust(4) +
+    "#{students[i][:country]}".ljust(11) +
+    "#{students[i][:cohort]}".ljust(11)
     i += 1
   end
-end
-def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+  puts "-----------------------------------------------"
 end
 
+def print_footer(students)
+  puts "Total students: #{students.count}"
+end
+
+program_intro
 students = input_students
 print_header
 print_list_of_all(students)
